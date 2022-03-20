c28a612f0e58862b2d98d6ec0db38d0578444e47
Mark Pokrovsky <solexofficial@yandex.ru>
2022-03-20 19:34:53 +0300

    feat: add fetch weather input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  const [query, setQuery] = useState('');
+
+  const search = async e => {
+    if (e.key === 'Enter') {
+      const data = await fetchWeather(query);
+
+      console.log(data);
+    }
+  };
+
+  return (
+    <div className='main-container'>
+      <input
+        type='text'
+        className='search'
+        placeholder='Search...'
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+    </div>
+  );
 };
 
 export default App;
